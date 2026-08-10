d48e36aa16346289ca0be5e1097ea960317d3b04
fantomsuj <sujay.kolpuru@gmail.com>
2026-08-10 11:22:33 -0700

    Use app-specific persistence store

--- Sources/NotionPiP/Persistence/NotionPiPPersistence.swift
+++ Sources/NotionPiP/Persistence/NotionPiPPersistence.swift
@@ -4,7 +4,8 @@ import SwiftData
 enum NotionPiPPersistence {
     static func makeContainer(
         storeURL: URL? = nil,
-        inMemory: Bool = false
+        inMemory: Bool = false,
+        applicationSupportDirectory: URL? = nil
     ) throws -> ModelContainer {
         let schema = Schema(versionedSchema: NotionPiPSchemaV4.self)
         let configuration: ModelConfiguration
@@ -17,7 +18,31 @@ enum NotionPiPPersistence {
         } else if let storeURL {
             configuration = ModelConfiguration(schema: schema, url: storeURL, cloudKitDatabase: .none)
         } else {
-            configuration = ModelConfiguration(schema: schema, cloudKitDatabase: .none)
+            let supportDirectory: URL
+            if let applicationSupportDirectory {
+                supportDirectory = applicationSupportDirectory
+            } else {
+                supportDirectory = try FileManager.default.url(
+                    for: .applicationSupportDirectory,
+                    in: .userDomainMask,
+                    appropriateFor: nil,
+                    create: true
+                )
+            }
+            let appDirectory = supportDirectory.appendingPathComponent(
+                "com.fantomsuj.NotionPiP",
+                isDirectory: true
+            )
+            try FileManager.default.createDirectory(
+                at: appDirectory,
+                withIntermediateDirectories: true
+            )
+            let appStoreURL = appDirectory.appendingPathComponent("NotionPiP.store")
+            configuration = ModelConfiguration(
+                schema: schema,
+                url: appStoreURL,
+                cloudKitDatabase: .none
+            )
         }
         return try ModelContainer(
             for: schema,
